6d41984f535b424275b9971d90bd818bb5a09bb6
alpha74 <amanalphakumar@gmail.com>
2020-09-05 02:26:54 +0530

    Add model Device.

--- device/models.py
+++ device/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Device( models.Model ):
+	uid = models.CharField( max_length = 12 )
+	name = models.TextField()
+	date_added = models.DateTimeField( default = timezone.now )
+	
+	def __str__( self ):
+		return self.uid + " " + self.name
+
+		
+	class Meta:
+		ordering = [ "uid" ]
+
