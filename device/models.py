62ef54978c2ceb2f1c249c0fe65a0407eebc94f5
alpha74 <amanalphakumar@gmail.com>
2020-09-05 02:36:59 +0530

    Add Temp and Humidity reading models.

--- device/models.py
+++ device/models.py
@@ -14,3 +14,22 @@ class Device( models.Model ):
 	class Meta:
 		ordering = [ "uid" ]
 
+
+
+class TemperatureReading( models.Model ):
+	uid = models.ForeignKey( Device, on_delete = models.CASCADE )
+	temperature = models.DecimalField( decimal_places = 4, max_digits = 7 )
+	timestamp = models.DateTimeField( default = timezone.now )
+	
+	def __str__( self ):
+		return self.uid + " " + self.temperature
+
+
+class HumidityReading( models.Model ):
+	uid = models.ForeignKey( Device, on_delete = models.CASCADE )
+	humidity = models.DecimalField( decimal_places = 2, max_digits = 5 )
+	timestamp = models.DateTimeField default = timezone.now )
+	
+	def __str__( self ):
+		return self.uid + " " + self.humidity
+	
