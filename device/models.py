40a32da26b7c2f5b110432c0b71a9a3df44b5257
alpha74 <amanalphakumar@gmail.com>
2020-09-05 02:55:06 +0530

    Create dir api.

--- device/models.py
+++ device/models.py
@@ -8,9 +8,9 @@ from django.utils import timezone
 		- date_added : date when device was added
 """
 class Device( models.Model ):
-	uid = models.CharField( max_length = 12 )
-	name = models.TextField()
-	date_added = models.DateTimeField( default = timezone.now )
+	uid = 			models.CharField( max_length = 12, null=False, blank=False )
+	name = 			models.TextField( max_length = 100 )
+	date_added = 	models.DateTimeField( default = timezone.now )
 	
 	def __str__( self ):
 		return self.uid + " " + self.name
@@ -27,9 +27,9 @@ class Device( models.Model ):
 		- timestamp : timestamp when reading was made
 """
 class TemperatureReading( models.Model ):
-	uid = models.ForeignKey( Device, on_delete = models.CASCADE )
-	temperature = models.DecimalField( decimal_places = 4, max_digits = 7 )
-	timestamp = models.DateTimeField( default = timezone.now )
+	uid = 			models.ForeignKey( Device, on_delete = models.CASCADE )
+	temperature = 	models.DecimalField( decimal_places = 4, max_digits = 7, null=False )
+	timestamp = 	models.DateTimeField( default = timezone.now )
 	
 	def __str__( self ):
 		return self.uid + " " + self.temperature
@@ -40,9 +40,9 @@ class TemperatureReading( models.Model ):
 	- timestamp : timestamp when reading was made
 """
 class HumidityReading( models.Model ):
-	uid = models.ForeignKey( Device, on_delete = models.CASCADE )
-	humidity = models.DecimalField( decimal_places = 2, max_digits = 5 )
-	timestamp = models.DateTimeField( default = timezone.now )
+	uid = 			models.ForeignKey( Device, on_delete = models.CASCADE )
+	humidity = 		models.DecimalField( decimal_places = 2, max_digits = 5, null=False )
+	timestamp = 	models.DateTimeField( default = timezone.now )
 	
 	def __str__( self ):
 		return self.uid + " " + self.humidity
