ae605b868e3cf7126266e08cec74f04089dc9e32
alpha74 <amanalphakumar@gmail.com>
2020-09-05 23:42:13 +0530

    Update api to Delete device.

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from django.core.exceptions import PermissionDenied, ValidationError
+from django.core.exceptions import PermissionDenied, ValidationError, ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
 
 from device.models import Device, TemperatureReading, HumidityReading
 from api.serializers import DeviceSerializer
@@ -56,21 +57,46 @@ def apiDevice( request ):
 
 """
 	- GET : Get details of all devices
+	- DELETE : Delete device
 """
-@api_view( ['GET',] )
-def apiDetailDevice( request, uid ):
-
-	try:
-		if( uid == "" ):
-			device_data = Device.objects.all()
-		else:
-			device_data = Device.objects.filter( uid = uid )
-		
-	except Device.DoesNotExist:
-		return Response( status = status.HTTP_404_NOT_FOUND )
-		
+@api_view( ['GET','DELETE',] )
+def apiGetDeleteDevice( request, uid ):
 	
-	if( request.method == "GET" ):
-		serializer = DeviceSerializer( device_data, many=True )
-		return Response( serializer.data )
+	# Get device details
+	if( request.method == "GET" ):	
+		try:
 		
+			if( uid == "" ):
+				device_data = Device.objects.all()
+			else:
+				device_data = get_object_or_404( Device, uid = uid )
+			
+		except Device.DoesNotExist:
+			return Response( status = status.HTTP_404_NOT_FOUND )
+			
+		except ObjectDoesNotExist:
+			return Response( status = status.HTTP_404_NOT_FOUND )
+		
+		if( request.method == "GET" ):
+			serializer = DeviceSerializer( device_data, many=True )
+			return Response( serializer.data )
+			
+	
+	# Delete device
+	elif( request.method == "DELETE" ):
+		try:			
+			# Get object with input uid
+			device_del = get_object_or_404( Device, uid = uid )
+			
+			# Delete the object
+			Device.objects.filter( uid = uid ).delete()
+		
+		except PermissionDenied as e:
+			return Response( status = status.HTTP_401_BAD_REQUEST )
+		
+		except ObjectDoesNotExist as e:
+			return Response( status = status.HTTP_404_NOT_FOUND )
+	 
+		if( request.method == "DELETE" ):
+			return Response( status = status.HTTP_202_ACCEPTED )
+			
