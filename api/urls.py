bb02180b9b4ee50e203ceb64b1a2c3dd0c5dc8ed
alpha74 <amanalphakumar@gmail.com>
2020-09-05 23:41:14 +0530

    Update route.

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from api.views import apiDevice, apiDetailDevice
+from api.views import apiDevice, apiGetDeleteDevice
 
 app_name = "airvd"
 
 urlpatterns = [
 	path ( "", apiDevice ),
-	path( "<str:uid>/", apiDetailDevice ),
+	path( "<str:uid>", apiGetDeleteDevice ),
 ]
