469b38e7c13d68c8b164022a7d6b8b6777206883
alpha74 <amanalphakumar@gmail.com>
2020-09-06 14:35:53 +0530

    Add route to GET readings.

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from api.views import apiDevice, apiGetDeleteDevice
+from api.views import apiDevice, apiGetDeleteDevice, apiGetReading
 
 app_name = "airvd"
 
 urlpatterns = [
 	path ( "", apiDevice ),
 	path( "<str:uid>", apiGetDeleteDevice ),
+	path( "<str:uid>/readings/<str:parameter>/", apiGetReading )
 ]
